e2516a7f1b9adbcfebe6068347503d810b3be8cb
Sergei Lukashin <lsv1387@gmail.com>
2023-05-23 15:16:17 +0300

    set active category

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 
+
+const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
+
+
 export const Categories = () => {
+  const [category, setCategory] = React.useState(categories[0])
+  
+  const onClickSetActiveCat = (activeCategory) => {
+    setCategory(activeCategory)
+  }
+  
   return (
     <nav className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map(cat => {
+          const activeCategoryClass = category === cat ? 'active' : ''
+          return (
+            <li key={cat}
+                onClick={onClickSetActiveCat.bind(this, cat)}
+                className={activeCategoryClass}>
+              {cat}
+            </li>
+          )
+        })}
       </ul>
     </nav>
   );
