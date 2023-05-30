cfaf667d28dbeaadaf6fb0fb2611e84a7378bc0c
Sergei Lukashin <lsv1387@gmail.com>
2023-05-31 01:42:39 +0300

    debounce input

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,40 +2,47 @@ import React from 'react';
 import s from './Search.module.scss'
 import {ReactComponent as SearchIcon} from "../../assets/img/search_icon.svg";
 import {ReactComponent as CloseIcon} from "../../assets/img/close_icon.svg";
-import {useDispatch, useSelector} from "react-redux";
+import debounce from 'lodash.debounce'
+import {useDispatch} from "react-redux";
 import {setInputValue} from "../../redux/slices/filterSlice";
 
 const Search = () => {
-  
-  const inputValue = useSelector(state => state.filter.inputValue)
+  const [localValue, setLocalValue] = React.useState('')
   const dispatch = useDispatch()
   const inputRef = React.useRef(null)
   
   const onClickClear = () => {
     if (inputRef) {
-      dispatch(setInputValue(''))
+      setLocalValue('')
       inputRef.current.focus()
     }
   }
   
-  const onChangeInputHandler = (e) => {
-    dispatch(setInputValue(e.currentTarget.value))
+  const updateSearchValue = React.useCallback(debounce((str) => {
+    dispatch(setInputValue(str))
+  }, 500), [])
+  
+  const onChangeInput = event => {
+    setLocalValue(event.currentTarget.value)
+    updateSearchValue(event.currentTarget.value)
   }
+  
   
   return (
     <div className={s.container}>
       <SearchIcon className={s.searchIco}/>
-      {inputValue
+      {localValue
         &&
         <div onClick={onClickClear}>
           <CloseIcon className={s.closeIco}/>
         </div>
       }
+      
       <input className={s.input}
              type="text"
              ref={inputRef}
-             value={inputValue}
-             onChange={onChangeInputHandler}
+             value={localValue}
+             onChange={onChangeInput}
              placeholder='Какую пиццу ищете?'/>
     </div>
   );
